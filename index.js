49a9c0c1b324363a800df95a76561c34af0690cc
edith147892 <medinaedith65@gmail.com>
2024-10-15 21:38:36 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
 
-export default function Home() {
-  const [count, setCount] = useState(0)
-
-  const handleScroll = () => {
-    setCount(count + scrollY)
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }
-  })
-
-  return (
-    <div className={styles.container}>
-      <p>You have moved {count} spots</p>
-      <button onClick={() => setCount(count + 1)}>
-        Move 1 spot
-      </button>
-    </div>
-  )
+function HomePage() {
+   return (
+      <>
+         <div>Welcome to Next.js!</div>
+         <Link href="/posts/first">First Post</Link>
+      </>	    
+   )
 }
+
+export default HomePage
+ 
